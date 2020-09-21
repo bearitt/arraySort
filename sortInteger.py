18539e21e1ed42439c48d6b9e0aaea93a7b3716a
Nate <43305251+N-a-t-e-w@users.noreply.github.com>
2020-09-20 21:12:33 -0700

    Completed sort

---
+++ sortInteger.py
@@ -0,0 +1,12 @@
+
+def sortIntegerList(list):
+
+    # INSERTION SORT
+    for i in range(0,len(list)):
+        item = list[i]
+        count = i - 1
+        while count >= 0 and (list[count] > item):
+            list[count+1] = list[count]
+            count -= 1
+        list[count+1] = item
+    return list
